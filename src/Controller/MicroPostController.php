f3832ab8c69948b9a4473900037d6021652c1068
DarkoKlisuric <44619056+DarkoKlisuric@users.noreply.github.com>
2019-07-21 20:55:23 +0200

    microPost

---
+++ src/Controller/MicroPostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/micro-post")
+ */
+class MicroPostController
+{
+    /**
+     * @Route("/" , name="micro_post_index")
+     */
+    public function index()
+    {
+
+    }
+}
