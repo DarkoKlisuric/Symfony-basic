f20300c3b2bf3fbcb9b599b5feb41614cb46722b
DarkoKlisuric <44619056+DarkoKlisuric@users.noreply.github.com>
2019-07-22 20:18:12 +0200

    unos podataka preko DataFixtures

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\MicroPostRepository;
+use http\Env\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,10 +12,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController
 {
     /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+    /**
+     * @var MicroPostRepository
+     */
+    private $microPostRepository;
+
+    public function __construct(\Twig_Environment $twig , MicroPostRepository $microPostRepository)
+    {
+        $this->twig = $twig;
+        $this->microPostRepository = $microPostRepository;
+    }
+
+    /**
      * @Route("/" , name="micro_post_index")
      */
     public function index()
     {
-
+        $html = $this->twig->render('micro-post/index.html.twig' ,
+            [
+                'posts' =>$this->microPostRepository->findAll()
+            ]);
+        return new Response($html);
     }
 }
