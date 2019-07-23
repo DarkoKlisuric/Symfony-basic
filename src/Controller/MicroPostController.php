db45a99f563eb1384932c00bb2ed96dfd945a4d5
DarkoKlisuric <44619056+DarkoKlisuric@users.noreply.github.com>
2019-07-23 21:17:46 +0200

    add Security

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
@@ -139,11 +140,15 @@ class MicroPostController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      * @Route("/add" , name="micro_post_add")
+     * @Security("is_granted('ROLE_USER')")
      */
-    public function add(Request $request)
+    public function add(Request $request ,TokenStorageInterface $tokenStorage)
     {
+        $user = $tokenStorage->getToken()->getUser();
+
         $microPost = new MicroPost();
         $microPost->setTime(new \DateTime());
+        $microPost->setUser($user);
 
         $form = $this->formFactory->create(MicroPostType::class , $microPost);
         $form->handleRequest($request);
